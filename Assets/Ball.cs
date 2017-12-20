0522821f0b90e8460b26d9031752413d41d0183a
DeeAndre Mac Andrew <34717537+Dstorm12@users.noreply.github.com>
2017-12-20 12:15:53 -0500

    Game files
    Transfered Unity Code

---
+++ Assets/Ball.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 0.1f;
+	public float drag = 0.1f;
+	public float deadZone =0.0015f;
+
+	public Vector3 position;
+	public Vector3 velocity;
+	public Vector3 acceleration;
+
+	public float speedMulti = 1f;
+	public float speedUpRate = 0.01f;
+
+	Camera cam;
+	float height;
+	float width;
+
+	public Text score;
+	public int p1 = 0;
+	public int p2 = 0;
+
+	// Use this for initialization
+	void Start () {
+			
+		Camera cam = Camera.main;
+		height = 2 * cam.orthographicSize;
+		width = height * cam.aspect;
+
+		recenter ();
+
+		score = GameObject.Find ("Score").GetComponent<Text> ();//Goes through the hierarchy, finds score and gets the text component
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Move ();
+
+	}
+
+	void Move(){
+
+		velocity = velocity + acceleration;
+		position = position + velocity * speed;
+
+		position = bounds (position);
+
+		this.gameObject.transform.position = position;
+	}
+
+	void recenter(){
+
+		acceleration = Vector3.zero;
+
+		if (Random.value > 0.5f)
+			velocity = new Vector3 (0.05f, Random.Range (0, .05f), 0f);
+		else
+			velocity = new Vector3 (-0.05f, Random.Range (0, 0.05f), 0f);
+
+		position = Vector3.zero;
+
+		speedMulti = 1;
+	}
+
+	Vector3 bounds(Vector3 pos){
+
+		//when you hit a wall
+		if (pos.y > height / 2) {
+			velocity = new Vector3 (velocity.x, -velocity.y, 0);
+		} else if (pos.y < -height / 2) {
+			velocity = new Vector3 (velocity.x, -velocity.y, 0);
+		}
+
+		//when you score
+		else if (pos.x > width / 2) {
+
+			p2++;
+			score.text = p2 + " - " + p1;
+			recenter ();
+			return Vector3.zero;
+		}
+		else if (pos.x < -width / 2) {
+
+			p1++;
+			score.text = p2 + " - " + p1;
+			recenter ();
+			return Vector3.zero;
+		}
+
+		return pos;
+	}
+
+	public void hit (Vector3 velo){
+		velocity = velo * speedMulti;
+		speedMulti = speedMulti + speedUpRate;
+	}
+}
---
+++ Assets/Paddle1.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paddle1 : MonoBehaviour {
+
+	public float speed = 1;
+	public float drag = 0.1f;
+	public float deadZone = 0.0015f;
+
+	Vector3 position;
+	Vector3 velocity;
+	Vector3 acceleration;
+
+	Camera cam;
+	float height;
+	float width;
+
+	public float maxVelocity = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+
+		Camera cam = Camera.main;
+		height = 2 * cam.orthographicSize;
+		width = height * cam.aspect;
+
+		acceleration = Vector3.zero;
+		velocity = Vector3.zero;
+		position = new Vector3 (width / 2, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ProcessInputs ();
+
+		Move ();
+	}
+
+	void ProcessInputs(){
+
+		if (Input.GetAxisRaw ("Vertical1") > 0) {
+			acceleration = Vector3.up * Time.deltaTime * speed;	//Time.deltaTime is the time taken to compute the last frame. This makes up for the difference in framerate.	
+		}
+
+		if (Input.GetAxisRaw ("Vertical1") < 0) {
+			acceleration = Vector3.up * Time.deltaTime * -speed;//Alternatively Vector3.down and no - sign
+		}
+
+		if (Input.GetAxisRaw ("Vertical1") == 0) {
+			acceleration = Vector3.zero;
+		}
+	}
+
+	void Move(){
+
+		velocity = velocity + acceleration;
+
+		if (Mathf.Abs(velocity.y) > maxVelocity) {
+			velocity.y = maxVelocity * Mathf.Sign (velocity.y);
+		} 
+		
+		position = position + velocity;
+
+		position = worldWrap (position);
+
+		this.gameObject.transform.position = position;
+	}
+
+	Vector3 worldWrap(Vector3 pos){
+
+		if (pos.y > height / 2) {
+			pos.y = -height / 2;
+		} else if (pos.y < -height / 2) {
+			pos.y = height / 2;
+		}
+
+		return pos;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){//When a collider enters another collider this function is automatically called
+
+		if (coll.gameObject.tag == "Ball") {//Checks to see that the collider has a tag called ball
+
+			var ball = coll.gameObject.GetComponent<Ball> ();//Looks for the parent of the collider and gets the component ball attached to the object
+			ball.hit (new Vector3 (-ball.velocity.x, ball.velocity.y, 0f));
+
+		}
+	}
+}
